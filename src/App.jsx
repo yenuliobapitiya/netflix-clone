47b30765419b13981369fa41e25f128a5f8f85c8
Yenuli Obapitiya <yenuliobapitiya@gmail.com>
2024-09-28 22:57:26 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Home from './pages/Home/Home'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import Login from './pages/Login/Login'
+import Player from './pages/Player/Player'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, async(user)=>{
+      if(user){
+        console.log("Logged In");
+        navigate('/');
+      }else{
+        console.log("Logged Out");
+        navigate('/login');
+      }
+    })
+  },[])
 
 export default App
