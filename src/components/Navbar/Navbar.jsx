2ea7b33ea7f69b722507545d8a7d1798faf4812f
Yenuli Obapitiya <yenuliobapitiya@gmail.com>
2024-09-29 00:00:51 +0530

    adding things

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.png'
 
 function Navbar() {
   return (
     <div className='navbar'>
-      
+      <div className="navbar-left">
+        <img src={logo} alt='' />
+      </div>
+      <div className="navbar-right"></div>
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Home.css'
+import Navbar from '../../components/Navbar/Navbar'
 
 function Home() {
   return (
     <div className='home'>
-      
+      <Navbar/>
     </div>
   )
 }
