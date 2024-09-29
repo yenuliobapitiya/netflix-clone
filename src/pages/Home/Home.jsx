e993fbb60dcc81ac9f952a8a7fb397b74b467095
Yenuli Obapitiya <yenuliobapitiya@gmail.com>
2024-09-29 17:49:45 +0530

    create home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
 import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
 
 function Home() {
   return (
@@ -9,6 +10,9 @@ function Home() {
       <Navbar/>
       <div className="hero">
         <img src={hero_banner} alt='' className='banner-img' />
+        <div className="hero_caption">
+          <img src={hero_title} alt='' />
+        </div>
       </div>
     </div>
   )
